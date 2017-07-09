8da2d730c678f1fba30925da6c70984cc1e122b3
chris <chris@bumblehead.com>
2017-07-08 19:19:24 -0700

    small improvement to makeclick

--- makeclick.sh
+++ makeclick.sh
@@ -5,9 +5,6 @@
 # $ adb push myclick_1.0.0_all.click /tmp
 # $ adb shell
 # $ pkcon install-local --allow-untrusted /tmp/myclick_1.0.0_all.click
-#
-# http://popey.com/blog/2015/07/28/easily-port-mobile-html5-games-to-ubuntu-phone/
-# http://www.linuxuk.org/post/20160518_snapping_electron_based_applications_simplenote/
 
 WEB_SRC=./src
 CLICK_DIR=./platforms/click
